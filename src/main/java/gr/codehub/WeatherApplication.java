b51defbb9c3c51d6bafa8635556025c23f22b7ee
Dimitris <iracleous@gmail.com>
2020-10-08 19:00:43 +0300

    Weather Service first commit

---
+++ src/main/java/gr/codehub/WeatherApplication.java
@@ -0,0 +1,17 @@
+package gr.codehub;
+
+
+import org.restlet.engine.Engine;
+import java.util.logging.Logger;
+
+
+public class WeatherApplication {
+
+    public static final Logger LOGGER = Engine.getLogger(WeatherApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("hello world");
+
+
+    }
+}
