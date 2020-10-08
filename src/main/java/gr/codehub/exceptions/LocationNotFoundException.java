ec6692bc5d4ef5deb79c2488e1525d266482c656
Dimitris <iracleous@gmail.com>
2020-10-08 20:16:38 +0300

    Weather Service using logger

---
+++ src/main/java/gr/codehub/exceptions/LocationNotFoundException.java
@@ -0,0 +1,9 @@
+package gr.codehub.exceptions;
+
+public class LocationNotFoundException extends Exception{
+
+    public LocationNotFoundException(String description)
+    {
+        super(description);
+    }
+}
